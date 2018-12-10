e3dde499efb6c8037877b3c756850788983f7c10
shao.cheng <shao.cheng@ttpai.cn>
2018-12-10 11:06:20 +0800

    Update Kafka常用.sql

--- Kafka常用.sql
+++ Kafka常用.sql
@@ -1,3 +1,13 @@
 
 --kafka查看log文件
 bin/kafka-run-class.sh kafka.tools.DumpLogSegments --files /tmp/kafka-logs/test3-0/00000000000000000000.log  --print-data-log
+
+--启动zookeeper
+bin/zookeeper-server-start.sh config/zookeeper.properties 
+
+--启动server
+bin/kafka-server-start.sh config/server.properties 
+
+--查看某个topic的日志
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic world
+
